575dda96dc3962810dab24ec396932d763b4da30
jerry <jeremy.morrison36@gmail.com>
2024-04-08 20:04:09 -0700

    update vscode-neovim config

--- dot_config/vscode-neovim/init.lua
+++ dot_config/vscode-neovim/init.lua
@@ -14,7 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   {
     "folke/flash.nvim",
-    -- event = "VeryLazy",
+    lazy = false,
     opts = {
       modes = {
         search = {
@@ -23,11 +23,10 @@ require("lazy").setup({
       }
     },
     keys = {
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      -- { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
-      -- { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
-      -- { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      -- { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+      { "r", mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+      { "R", mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     }
   },
   {
@@ -45,111 +44,86 @@ require("lazy").setup({
     opts = {},
     lazy = false,
     config = function()
-        local ft = require('Comment.ft')
-        ft.set('kbd', ft.get('lisp'))
-        -- require('Comment').setup()
+      local ft = require('Comment.ft')
+      ft.set('kbd', ft.get('lisp'))
+      -- require('Comment').setup()
+    end,
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        -- Add languages to be installed here that you want installed for treesitter
+        ensure_installed = { 'lua', 'python', 'bash', 'markdown' },
+
+        -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+        auto_install = false,
+
+        highlight = { enable = true },
+        indent = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = '<c-space>',
+            node_incremental = '<c-space>',
+            scope_incremental = '<c-s>',
+            node_decremental = '<M-space>',
+          },
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ['aa'] = '@parameter.outer',
+              ['ia'] = '@parameter.inner',
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.inner',
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              [']m'] = '@function.outer',
+              [']]'] = '@class.outer',
+            },
+            goto_next_end = {
+              [']M'] = '@function.outer',
+              [']['] = '@class.outer',
+            },
+            goto_previous_start = {
+              ['[m'] = '@function.outer',
+              ['[['] = '@class.outer',
+            },
+            goto_previous_end = {
+              ['[M'] = '@function.outer',
+              ['[]'] = '@class.outer',
+            },
+          },
+          swap = {
+            enable = true,
+            swap_next = {
+              ['<leader>a'] = '@parameter.inner',
+            },
+            swap_previous = {
+              ['<leader>A'] = '@parameter.inner',
+            },
+          },
+        },
+      }
     end,
   }
 })
--- {
---   'nvim-treesitter/nvim-treesitter',
---   build = ':TSUpdate',
---   config = function()
---     require('nvim-treesitter.configs').setup {
---       -- Add languages to be installed here that you want installed for treesitter
---       ensure_installed = { 'lua', 'python', 'bash' },
---
---       -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
---       auto_install = false,
---
---       highlight = { enable = true },
---       indent = { enable = true },
---       incremental_selection = {
---         enable = true,
---         keymaps = {
---           init_selection = '<c-space>',
---           node_incremental = '<c-space>',
---           scope_incremental = '<c-s>',
---           node_decremental = '<M-space>',
---         },
---       },
---       textobjects = {
---         select = {
---           enable = true,
---           lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
---           keymaps = {
---             -- You can use the capture groups defined in textobjects.scm
---             ['aa'] = '@parameter.outer',
---             ['ia'] = '@parameter.inner',
---             ['af'] = '@function.outer',
---             ['if'] = '@function.inner',
---             ['ac'] = '@class.outer',
---             ['ic'] = '@class.inner',
---           },
---         },
---         move = {
---           enable = true,
---           set_jumps = true, -- whether to set jumps in the jumplist
---           goto_next_start = {
---             [']m'] = '@function.outer',
---             [']]'] = '@class.outer',
---           },
---           goto_next_end = {
---             [']M'] = '@function.outer',
---             [']['] = '@class.outer',
---           },
---           goto_previous_start = {
---             ['[m'] = '@function.outer',
---             ['[['] = '@class.outer',
---           },
---           goto_previous_end = {
---             ['[M'] = '@function.outer',
---             ['[]'] = '@class.outer',
---           },
---         },
---         swap = {
---           enable = true,
---           swap_next = {
---             ['<leader>a'] = '@parameter.inner',
---           },
---           swap_previous = {
---             ['<leader>A'] = '@parameter.inner',
---           },
---         },
---       },
---     }
---   end,
--- }
--- })
 
 vim.g.mapleader = " "
 vim.o.clipboard = 'unnamedplus'
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
--- TODO add condition to check env so it doesn't cause errors when I debug from neovim
-local vscode = require("vscode-neovim")
-
-vim.keymap.set("", "<a-h>", function()
-  vscode.action("workbench.action.navigateLeft")
-end)
-vim.keymap.set("", "<a-j>", function()
-  vim.fn.VSCodeNotify("workbench.action.navigateDown")
-end)
-vim.keymap.set("", "<a-k>", function()
-  vim.fn.VSCodeNotify("workbench.action.navigateUp")
-end)
-vim.keymap.set("", "<a-l>", function()
-  vim.fn.VSCodeNotify("workbench.action.navigateRight")
-end)
-
--- Remap for dealing with word wrap -- not working
--- vim.keymap.set('', 'k', "gk")
--- vim.keymap.set('', 'j', "gj", { noremap = false })
-
-vim.keymap.set("n", "<leader>z", function()
-  vscode.action("workbench.action.toggleZenMode")
-end)
 
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
@@ -159,3 +133,19 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
+
+-- vscode configuration below - return if not in vscode so we can debug from
+-- neovim without errors
+if not vim.g.vscode then
+  return
+end
+
+local vscode = require("vscode-neovim")
+
+-- this works but you can't prefix it with number :(
+-- vim.keymap.set('n', 'k', function()
+--   vscode.action('cursorMove', { args = { to = 'up', by = 'wrappedLine' } })
+-- end)
+-- vim.keymap.set('n', 'j', function()
+--   vscode.action('cursorMove', { args = { to = 'down', by = 'wrappedLine' } })
+-- end)
