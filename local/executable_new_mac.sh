466e3af70fcde79726f3e27a52a28cbcf867ce0a
jerry <jeremy.morrison36@gmail.com>
2024-04-29 11:52:08 -0700

    update macos packages

---
+++ local/executable_new_mac.sh
@@ -0,0 +1,21 @@
+# document commands to run on a new macos installation
+
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install brew packages
+brew bundle
+
+# install rust and cargo
+curl https://sh.rustup.rs -sSf | sh
+
+# install cargo packages
+source ./cargo_package_install.sh
+
+# install pipx packages
+set -ux
+if [[ -e ./pipx_packages.json ]]; then
+	for p in $(cat ./pipx_packages.json | jq -r '.venvs[].metadata.main_package.package_or_url'); do
+		pipx install $p
+	done
+fi
