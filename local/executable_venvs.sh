b9fb5d252bf6c01a8db9951a976b5093af1613a2
jerry <jeremy.morrison36@gmail.com>
2024-06-17 12:11:33 -0700

    remove pynvim and syncall virtualenvs

--- local/executable_venvs.sh
+++ local/executable_venvs.sh
@@ -17,25 +17,3 @@ pip install 'ibis-framework[duckdb]'
 pip install 'ibis-framework[postgres]'
 
 deactivate
-
-# pynvim
-# do not use uv for pynvim
-cd ~/venvs
-mkdir pynvim
-cd pynvim
-mise shell python@latest
-python -m venv .venv
-source .venv/bin/activate
-pip install pynvim
-
-deactivate
-
-# syncall
-cd ~/venvs
-mkdir syncall
-mise shell python@3.11
-python -m venv .venv
-source .venv/bin/activate
-pip install syncall[google,tw]
-
-deactivate
