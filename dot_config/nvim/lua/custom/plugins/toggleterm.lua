b6f22874eb04bc8648809efa534b00d8f5ea5ba3
jerry <jeremy.morrison36@gmail.com>
2024-06-22 15:26:20 -0700

    floating terminal instead of horizontal

--- dot_config/nvim/lua/custom/plugins/toggleterm.lua
+++ dot_config/nvim/lua/custom/plugins/toggleterm.lua
@@ -2,12 +2,12 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   opts = {
-    direction = 'horizontal',
-    size = 20,
+    direction = 'float',
+    -- size = 20,
     open_mapping = ';j',
     insert_mappings = false,
     -- this shouldn't cause too many problems in terminal mode. ;j is not common.
     terminal_mappings = true,
-    shade_terminals = false,
+    shade_terminals = true,
   },
 }
