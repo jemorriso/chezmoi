b9661c9bd120682ce66c2c5efe35f2b28f0ac54c
jerry <jeremy.morrison36@gmail.com>
2024-06-25 11:47:05 -0700

    update github repos

--- local/executable_github_repo_clone.sh
+++ local/executable_github_repo_clone.sh
@@ -2,10 +2,13 @@ cd
 mkdir github
 cd github
 gh repo clone Aloxaf/fzf-tab Aloxaf/fzf-tab
+gh repo clone ageron/handson-ml3 ageron/handson-ml3
 gh repo clone catppuccin/alacritty catppuccin/alacritty
 gh repo clone catppuccin/bat catppuccin/bat
+gh repo clone catppuccin/kitty catppuccin/kitty
 gh repo clone catppuccin/sioyek catppuccin/sioyek
-gh repo clone cnstlungu/portable-data-stack-dagster cnstlungu/portable-data-stack-dagster
-gh repo clone dagster-io/dagster-open-platform dagster-io/dagster-open-platform
+gh repo clone catppuccin/spicetify catppuccin/spicetify
+gh repo clone catppuccin/vimium catppuccin/vimium
 gh repo clone fracnesco/hackyourdaw fracnesco/hackyourdaw
+gh repo clone ohmyzsh/ohmyzsh ohmyzsh/ohmyzsh
 gh repo clone saulpw/visidata saulpw/visidata
