d1915a21fd0db03ce59f44ae9624fc0b0e150a5a
jerry <jeremy.morrison36@gmail.com>
2024-06-28 17:24:22 -0700

    don't delete manually added frontmatter

--- dot_config/nvim/lua/custom/plugins/obsidian.lua
+++ dot_config/nvim/lua/custom/plugins/obsidian.lua
@@ -15,6 +15,15 @@ return {
     ---@return table
     note_frontmatter_func = function(note)
       local out = { tags = note.tags }
+
+      -- `note.metadata` contains any manually added fields in the frontmatter.
+      -- So here we just make sure those fields are kept in the frontmatter.
+      if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+        for k, v in pairs(note.metadata) do
+          out[k] = v
+        end
+      end
+
       return out
     end,
   },
